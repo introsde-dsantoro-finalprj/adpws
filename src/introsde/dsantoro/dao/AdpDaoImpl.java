e2241f7dafd46b99f98cb294ba0b66468785b058
Daniele Santoro <psyacca@gmail.com>
2016-02-05 19:36:17 +0100

    filter only needed information fof the product from foodessentials API

--- src/introsde/dsantoro/dao/AdpDaoImpl.java
+++ src/introsde/dsantoro/dao/AdpDaoImpl.java
@@ -1,5 +1,6 @@
 package introsde.dsantoro.dao;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import introsde.dsantoro.foodessentials.FoodessentialsClient;
@@ -9,8 +10,18 @@ public class AdpDaoImpl implements AdpDao {
 	@Override
 	public JSONObject searchMeals(String key, int start, int num) {
 		FoodessentialsClient foodClient = new FoodessentialsClient();
-		return foodClient.searchFoods(key, start, num);
-//		return null;
+		JSONArray prodArray = foodClient.searchFoods(key, start, num).getJSONArray("productsArray");
+				
+		for (int i = 0 ; i < prodArray.length(); i++) {
+	        JSONObject obj = prodArray.getJSONObject(i);
+	        String prodName = obj.getString("product_name");
+	        String prodUpc = obj.getString("upc");
+	        obj = (JSONObject) getMeal(prodUpc).getJSONObject("product").getJSONArray("nutrients").getJSONObject(0);
+	        System.out.println(prodName);
+	        System.out.println(prodUpc);
+	        System.out.println(obj.getString("nutrient_value"));	        
+	    }
+		return null;
 	}
 
 	@Override
