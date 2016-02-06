8d145f559e1173bf3265309b147dd85a4ba8d4ac
Daniele Santoro <psyacca@gmail.com>
2016-02-06 10:12:39 +0100

    check FOOD_APIKEY and PORT when publishing service

--- src/introsde/dsantoro/endpoint/AdpwsPublisher.java
+++ src/introsde/dsantoro/endpoint/AdpwsPublisher.java
@@ -3,35 +3,41 @@ package introsde.dsantoro.endpoint;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+
 import java.net.InetAddress;
 
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
 
 public class AdpwsPublisher {
-    public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException
-    {
-        String protocol = "http://";
-        String port_value = "5700";
-        if (String.valueOf(System.getenv("PORT")) != "null"){
-            port_value=String.valueOf(System.getenv("PORT"));
-        }
-        String port = ":"+port_value+"/";
-        String hostname = InetAddress.getLocalHost().getHostAddress();
-        if (hostname.equals("127.0.0.1"))
-        {
-            hostname = "localhost";
-        }
+	public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException
+	{
+		String PROTOCOL = "http://";
+		String HOSTNAME = InetAddress.getLocalHost().getHostAddress();
+		if (HOSTNAME.equals("127.0.0.1"))
+		{
+			HOSTNAME = "localhost";
+		}
+		String PORT = "6904";
+		String BASE_URL = "/ws/adpws";
 
-        URI BASE_URI = new URI(protocol + hostname + port+"ws/adpws/");
+		if (String.valueOf(System.getenv("PORT")) != "null"){
+			PORT=String.valueOf(System.getenv("PORT"));
+		}
 
-        System.out.println("Starting sdelab standalone HTTP server...");
-        JdkHttpServerFactory.createHttpServer(BASE_URI, createApp());
-        System.out.println("Server started on " + BASE_URI + "\n[kill the process to exit]");
-    }
+		System.out.println("Starting adapter Service...");
 
-    public static ResourceConfig createApp() {
-        System.out.println("Starting sdelab REST services...");
-        return new AdpwsConfig();
-    }
+		// Check setting variables for south-bound services        
+		String apikey = System.getenv("FOOD_APIKEY");
+		if (apikey != null) {
+			URI BASE_URI = new URI(PROTOCOL + HOSTNAME + ":" + PORT + BASE_URL);	        
+			JdkHttpServerFactory.createHttpServer(BASE_URI, new AdpwsConfig());
+			System.out.println("--> Published. Check out "+BASE_URI);
+		}
+		else {
+			// Cannot connect to dependent services
+			System.out.println("--> ERROR: Not published. Check out dependent services:");
+			System.out.println("----> FOO_APIKEY: " + apikey);
+		}        
+	}
+
 }
